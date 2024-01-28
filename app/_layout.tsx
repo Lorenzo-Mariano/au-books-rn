91947418f135207ce5279a3340742ab4c8e5bc22
Lorenzo-Mariano <hhumminah@gmail.com>
2024-01-28 18:19:56 +0800

    Added icons to drawer items

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
 	DarkTheme,
@@ -10,6 +11,10 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 
 import { useColorScheme } from "@/components/useColorScheme";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Ionicons } from "@expo/vector-icons";
+
+import CustomDrawerContent from "../components/CustomDrawerContent";
 
 export {
 	// Catch any errors thrown by the Layout component.
@@ -18,7 +23,7 @@ export {
 
 export const unstable_settings = {
 	// Ensure that reloading on `/modal` keeps a back button present.
-	initialRouteName: "(tabs)",
+	initialRouteName: "Home",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -53,103 +58,178 @@ function RootLayoutNav() {
 
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<Drawer>
-				<Drawer.Screen
-					name="Home"
-					options={{
-						drawerLabel: "College",
-						headerTitle: "College",
-					}}
-				></Drawer.Screen>
+			<GestureHandlerRootView style={{ flex: 1 }}>
+				{/* <Drawer drawerContent={CustomDrawerContent}> */}
+				<Drawer>
+					<Drawer.Screen
+						name="Home"
+						options={{
+							drawerLabel: "College",
+							headerTitle: "College",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="home-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Services"
-					options={{
-						drawerLabel: "Services",
-						headerTitle: "Services",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Services"
+						options={{
+							drawerLabel: "Services",
+							headerTitle: "Services",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="bag-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Uniform"
-					options={{
-						drawerLabel: "Uniform",
-						headerTitle: "Uniform",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Uniform"
+						options={{
+							drawerLabel: "Uniform",
+							headerTitle: "Uniform",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="shirt-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Map"
-					options={{
-						drawerLabel: "Map",
-						headerTitle: "Map",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Map"
+						options={{
+							drawerLabel: "Map",
+							headerTitle: "Map",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="map-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Scholarship"
-					options={{
-						drawerLabel: "Scholarship",
-						headerTitle: "Scholarship",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Scholarship"
+						options={{
+							drawerLabel: "Scholarship",
+							headerTitle: "Scholarship",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="school-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Event"
-					options={{
-						drawerLabel: "Event",
-						headerTitle: "Event",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Event"
+						options={{
+							drawerLabel: "Event",
+							headerTitle: "Event",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="bonfire-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="StudentManual"
-					options={{
-						drawerLabel: "Student Manual",
-						headerTitle: "Student Manual",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="StudentManual"
+						options={{
+							drawerLabel: "Student Manual",
+							headerTitle: "Student Manual",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="book-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Faq"
-					options={{
-						drawerLabel: "FAQ",
-						headerTitle: "FAQ",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Faq"
+						options={{
+							drawerLabel: "FAQ",
+							headerTitle: "FAQ",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons
+										name="help-circle-outline"
+										size={size}
+										color={color}
+									/>
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="About"
-					options={{
-						drawerLabel: "About",
-						headerTitle: "About",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="About"
+						options={{
+							drawerLabel: "About",
+							headerTitle: "About",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons
+										name="information-circle-outline"
+										size={size}
+										color={color}
+									/>
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="PhinmaEducation"
-					options={{
-						drawerLabel: "PHINMA Education",
-						headerTitle: "PHINMA Education",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="PhinmaEducation"
+						options={{
+							drawerLabel: "PHINMA Education",
+							headerTitle: "PHINMA Education",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="business-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Developers"
-					options={{
-						drawerLabel: "Developers",
-						headerTitle: "Developers",
-					}}
-				></Drawer.Screen>
+					<Drawer.Screen
+						name="Developers"
+						options={{
+							drawerLabel: "Developers",
+							headerTitle: "Developers",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons
+										name="code-slash-outline"
+										size={size}
+										color={color}
+									/>
+								);
+							},
+						}}
+					></Drawer.Screen>
 
-				<Drawer.Screen
-					name="Contact"
-					options={{
-						drawerLabel: "Contact",
-						headerTitle: "Contact",
-					}}
-				></Drawer.Screen>
-			</Drawer>
+					<Drawer.Screen
+						name="Contact"
+						options={{
+							drawerLabel: "Contact",
+							headerTitle: "Contact",
+							drawerIcon: ({ size, color }) => {
+								return (
+									<Ionicons name="call-outline" size={size} color={color} />
+								);
+							},
+						}}
+					></Drawer.Screen>
+				</Drawer>
+			</GestureHandlerRootView>
 		</ThemeProvider>
 	);
 }
