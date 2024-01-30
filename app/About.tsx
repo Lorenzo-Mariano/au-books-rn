20eaa8a3d4d8e27d53763aaf647a6a70a76f202f
Lorenzo-Mariano <hhumminah@gmail.com>
2024-01-30 17:18:32 +0800

    Finished about tab.

--- app/About.tsx
+++ app/About.tsx
@@ -1,22 +1,54 @@
-import { StyleSheet, Text, View, useColorScheme } from "react-native";
+import { Image, StyleSheet, Text, View, useColorScheme } from "react-native";
 import React from "react";
 
 export default function About() {
 	const colorScheme = useColorScheme();
 	const fontColor = colorScheme === "dark" ? "#bababa" : "#000";
 	return (
-		<View>
-			<Text>About</Text>
+		<View style={styles.mainView}>
+			<Text style={[styles.biggerText, styles.headline, { color: fontColor }]}>
+				Stay up to date
+			</Text>
+			<Image
+				style={styles.img}
+				source={require("../assets/images/about/books.jpg")}
+			/>
+			<Text style={{ fontSize: 16, color: fontColor }}>
+				This app is for keeping track of books borrowed from the library. Don't
+				miss it the next time your favorite books become available again!
+			</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	mainView: {
+		flex: 1,
+		flexDirection: "column",
+		gap: 10,
 		padding: "5%",
 	},
 
+	headline: {
+		marginBottom: 30,
+		fontWeight: "800",
+		paddingBottom: 10,
+		borderBottomColor: "#a1b8ff",
+		borderBottomWidth: 2,
+	},
+
+	biggerText: {
+		fontSize: 26,
+	},
+
 	bigText: {
-		fontSize: 24,
+		fontSize: 20,
+	},
+
+	img: {
+		height: undefined,
+		width: "100%",
+		aspectRatio: 1000 / 667,
+		borderRadius: 15,
 	},
 });
--- app/Services.tsx
+++ app/Services.tsx
@@ -3,16 +3,7 @@ import { StyleSheet, Text, View, Image, useColorScheme } from "react-native";
 export default function Services() {
 	const colorScheme = useColorScheme();
 	const fontColor = colorScheme === "dark" ? "#bababa" : "#000";
-	return (
-		<View style={styles.mainView}>
-			<Image
-				source={{
-					uri: "https://reactnative.dev/img/tiny_logo.png",
-				}}
-			/>
-			<Text>Services</Text>
-		</View>
-	);
+	return <View style={styles.mainView}></View>;
 }
 
 const styles = StyleSheet.create({
@@ -20,7 +11,19 @@ const styles = StyleSheet.create({
 		padding: "5%",
 	},
 
+	headline: {
+		marginBottom: 30,
+		fontWeight: "800",
+		paddingBottom: 10,
+		borderBottomColor: "#a1b8ff",
+		borderBottomWidth: 2,
+	},
+
+	biggerText: {
+		fontSize: 26,
+	},
+
 	bigText: {
-		fontSize: 24,
+		fontSize: 20,
 	},
 });
