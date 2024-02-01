e6d3fb8a95fa418ddf00fd04aa712e81751b5bd7
Lorenzo-Mariano <hhumminah@gmail.com>
2024-02-02 05:51:44 +0800

    student manual now uses webview; is clearer and zoomable.

--- app/StudentManual.tsx
+++ app/StudentManual.tsx
@@ -1,26 +1,25 @@
 import { Image, StyleSheet, Text, View, useColorScheme } from "react-native";
 import React from "react";
 import { ScrollView } from "react-native-gesture-handler";
+import { WebView } from "react-native-webview";
 
 export default function StudentManual() {
 	const colorScheme = useColorScheme();
 	const fontColor = colorScheme === "dark" ? "#bababa" : "#000";
+	const manualUrl = require("../assets/images/studentmanual/student manual.jpg");
+
 	return (
-		<ScrollView>
-			<View>
-				<Image
-					resizeMethod="resize"
-					style={styles.img}
-					source={require("../assets/images/studentmanual/student manual.jpg")}
-				/>
-			</View>
-		</ScrollView>
+		<WebView
+			originWhitelist={["*"]}
+			// other solution was to pass uri of discord image lmao
+			source={require("../assets/images/studentmanual/student manual.jpg")}
+		/>
 	);
 }
 
 const styles = StyleSheet.create({
 	img: {
-		width: "100%",
+		width: "60%",
 		height: undefined,
 		aspectRatio: 971 / 11708,
 	},
