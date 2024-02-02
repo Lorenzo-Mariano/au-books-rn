808b4c1e0866b52a2ad1fe5eb51139c3d9d9630e
Lorenzo-Mariano <hhumminah@gmail.com>
2024-02-02 13:19:40 +0800

    switched to using remote file uri in student manual webview

--- app/StudentManual.tsx
+++ app/StudentManual.tsx
@@ -12,15 +12,12 @@ export default function StudentManual() {
 		<WebView
 			originWhitelist={["*"]}
 			// other solution was to pass uri of discord image lmao
-			source={require("../assets/images/studentmanual/student manual.jpg")}
+			source={{
+				uri: "https://cdn.discordapp.com/attachments/959064774610661416/1202727528201191424/student_manual.jpg?ex=65ce824c&is=65bc0d4c&hm=f5fd736400baf5c4321fcd95ee6a4aeb958ce91aee18a82700ee95c0b4454902&",
+			}}
+			// source={require("../assets/images/studentmanual/student manual.jpg")}
 		/>
 	);
 }
 
-const styles = StyleSheet.create({
-	img: {
-		width: "60%",
-		height: undefined,
-		aspectRatio: 971 / 11708,
-	},
-});
+const styles = StyleSheet.create({});
